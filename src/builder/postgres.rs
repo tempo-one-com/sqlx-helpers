8a7f2a15eac5eb520f27480cd9ed84a5d2f50b30
Olivier Poppon <olivier.poppon@tempo-one.com>
2024-04-12 18:01:32 +0200

    ajout and_starts_like

--- src/builder/postgres.rs
+++ src/builder/postgres.rs
@@ -1,32 +1,44 @@
 use std::fmt::Display;
 
-use sqlx::{QueryBuilder, Postgres};
+use sqlx::{Postgres, QueryBuilder};
 
 use super::ValueType;
 
 pub struct Builder<'a> {
-    pub internal: QueryBuilder<'a, Postgres>
+    pub internal: QueryBuilder<'a, Postgres>,
 }
 
 impl<'a> Builder<'a> {
     pub fn new(init: impl Into<String>) -> Self {
         Builder {
-            internal: QueryBuilder::new(init)
+            internal: QueryBuilder::new(init),
         }
     }
 
     pub fn push(&mut self, sql: impl Display) -> &mut Self {
         self.internal.push(format!(" {sql} "));
-        
+
         self
     }
 
     pub fn and(&mut self, sql: &str, value: ValueType) -> &mut Self {
         match value {
-            ValueType::None => {},
+            ValueType::None => {}
             _ => {
                 self.internal.push(format!(" AND {sql} = "));
-                self.bind(value);        
+                self.bind(value);
+            }
+        };
+
+        self
+    }
+
+    pub fn and_starts_like(&mut self, sql: &str, value: ValueType) -> &mut Self {
+        match value {
+            ValueType::None => {}
+            _ => {
+                self.internal.push(format!(" AND {sql} LIKE "));
+                self.bind(value);
             }
         };
 
@@ -37,10 +49,10 @@ impl<'a> Builder<'a> {
         match value {
             ValueType::String(x) => self.internal.push_bind(x),
             ValueType::Int(x) => self.internal.push_bind(x),
-            ValueType::Float(x) => self.internal.push_bind(x),            
+            ValueType::Float(x) => self.internal.push_bind(x),
             ValueType::Bool(x) => self.internal.push_bind(x),
-            ValueType::Date(x) => self.internal.push_bind(x),        
-            ValueType::DateTime(x) => self.internal.push_bind(x),                    
+            ValueType::Date(x) => self.internal.push_bind(x),
+            ValueType::DateTime(x) => self.internal.push_bind(x),
             ValueType::None => &self.internal,
         };
     }
@@ -53,9 +65,9 @@ mod tests {
     #[test]
     fn string() {
         let mut builder = Builder {
-            internal: QueryBuilder::new("")
+            internal: QueryBuilder::new(""),
         };
-        
+
         builder.and("field", "value".to_string().into());
 
         assert_eq!(builder.internal.into_sql(), " AND field = $1")
@@ -64,7 +76,7 @@ mod tests {
     #[test]
     fn int() {
         let mut builder = Builder::new("");
-        
+
         builder.and("field", 42.into());
 
         assert_eq!(builder.internal.into_sql(), " AND field = $1")
@@ -73,22 +85,22 @@ mod tests {
     #[test]
     fn some() {
         let mut builder = Builder::new("");
-        
+
         builder.and("field", Some("1111".to_string()).into());
 
         assert_eq!(builder.internal.into_sql(), " AND field = $1")
-    }    
+    }
 
     #[test]
     fn none() {
         let mut builder = Builder::new("");
 
-        let value:Option<String> = None;
+        let value: Option<String> = None;
 
         builder.and("field", value.into());
 
         assert_eq!(builder.internal.into_sql(), "")
-    }    
+    }
 
     #[test]
     fn push() {
@@ -96,5 +108,16 @@ mod tests {
         builder.push("SELECT * FROM");
 
         assert_eq!(builder.internal.into_sql(), " SELECT * FROM ")
+    }
+
+    #[test]
+    fn starts_like() {
+        let mut builder = Builder::new("");
+        let name_like = "hank";
+        let name_like = format!("{name_like}%");
+
+        builder.and_starts_like("field", name_like.into());
+
+        assert_eq!(builder.internal.into_sql(), " AND field LIKE $1")
     }
 }
