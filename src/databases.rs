0c5d80e005e258872b0ccfad08ea957a46c28573
Olivier Poppon <olivier.poppon@tempo-one.com>
2024-12-02 16:28:55 +0100

    gestion de pas de postgres local

--- src/databases.rs
+++ src/databases.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashMap, env::Vars};
 use sqlx::{mysql::MySqlPoolOptions, postgres::PgPoolOptions, MySqlPool, PgPool};
+use std::{collections::HashMap, env::Vars};
 
 use crate::DATABASE_URL;
 
@@ -11,15 +11,23 @@ struct Teliway {
 
 #[derive(Clone, Debug)]
 pub struct Databases {
-    pub default: PgPool,
+    pub default: Option<PgPool>,
     pub teliways: HashMap<String, MySqlPool>,
 }
 
 impl Databases {
-    pub async fn init(vars: Vars, default_max_connections: u32, teliway_max_connections: u32) -> Result<Self, sqlx::Error> {
+    pub async fn init(
+        vars: Vars,
+        default_max_connections: u32,
+        teliway_max_connections: u32,
+    ) -> Result<Self, sqlx::Error> {
         let (default_url, teliways_urls) = get_teliways_codes_from_env(vars);
 
-        let default = Databases::init_default(default_url, default_max_connections).await?;
+        let default = match default_url {
+            Some(url) => Some(Databases::init_default(url, default_max_connections).await?),
+            _ => None,
+        };
+
         let teliways = Databases::init_teliways(teliways_urls, teliway_max_connections).await;
 
         Ok(Databases { default, teliways })
@@ -32,7 +40,10 @@ impl Databases {
             .await
     }
 
-    async fn init_teliways(codes: Vec<(String, String)>, max_connections: u32) -> HashMap<String, MySqlPool> {
+    async fn init_teliways(
+        codes: Vec<(String, String)>,
+        max_connections: u32,
+    ) -> HashMap<String, MySqlPool> {
         let futures = codes
             .into_iter()
             .map(|(code, url)| Databases::init_teliway(code, url, max_connections))
@@ -47,7 +58,11 @@ impl Databases {
             .collect::<HashMap<_, _>>()
     }
 
-    async fn init_teliway(code: String, url: String, max_connections: u32) -> Result<Teliway, sqlx::Error> {
+    async fn init_teliway(
+        code: String,
+        url: String,
+        max_connections: u32,
+    ) -> Result<Teliway, sqlx::Error> {
         let pool = MySqlPoolOptions::new()
             .max_connections(max_connections)
             .connect(&url)
@@ -57,23 +72,21 @@ impl Databases {
     }
 
     pub fn get_by_code(&self, code: &str) -> Option<MySqlPool> {
-        self.teliways
-            .get(code)
-            .cloned()
+        self.teliways.get(code).cloned()
     }
 }
 
-fn get_teliways_codes_from_env(vars: Vars) -> (String, Vec<(String, String)>) {
+fn get_teliways_codes_from_env(vars: Vars) -> (Option<String>, Vec<(String, String)>) {
     const PREFIX: &str = "DATABASE_";
     const SUFFIX: &str = "_URL";
 
-    let mut default = String::new();
+    let mut default = None;
     let mut teliways = vec![];
 
     for (key, value) in vars {
         if key.starts_with(PREFIX) {
             if key == DATABASE_URL {
-                default = value;
+                default = Some(value);
             } else if let Some(code) = key
                 .strip_prefix(PREFIX)
                 .and_then(|x| x.strip_suffix(SUFFIX))
@@ -98,7 +111,7 @@ mod tests {
         env::set_var("DATABASE_GTRA_URL", "tw_gtra");
         let (default, teliways) = get_teliways_codes_from_env(env::vars());
 
-        assert_eq!(default, "onex");
+        assert_eq!(default, Some("onex".to_string()));
         assert_eq!(teliways[0], ("gtra".to_string(), "tw_gtra".to_string()));
     }
 }
